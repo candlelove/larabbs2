f1fd59b6963ef4ccf4a54c5e5444bf7c2dee5922
candlelove <77607022@qq.com>
2021-12-29 18:19:28 +0800

    修复跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    //public const HOME = '/home';默认
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
